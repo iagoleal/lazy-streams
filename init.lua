4d013b2ef7bc9693d57dfbec2631ece044717e7f
iago <iago.lealf@gmail.com>
2021-04-04 14:40:01 -0300

    Module environment

--- init.lua
+++ init.lua
@@ -1,4 +1,35 @@
+--[[
+  MIT License
 
+  Copyright (c) 2021 Iago Leal de Freitas
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+--]]
+
+-- Setup the environment
+local M = {__version = "0.1.0"}
+setmetatable(M, {__index = _G})
+if setfenv then
+  setfenv(1, M) -- for 5.1 / luajit
+else
+  _ENV = M      -- for 5.2+
+end
 
 -- Constant function
 function const(x)
@@ -236,6 +267,27 @@ pp = function(t)
   io.write('[')
   for _, v in ipairs(t) do
     io.write(tostring(v), ", ")
+
+-----------------------
+-- Set methods
+-----------------------
+
+do
+  local mt   = methods
+  mt.head    = head
+  mt.tail    = tail
+  mt.uncons  = uncons
+  mt.map     = function(self, f)
+    return map(f, self)
   end
   io.write(']\n')
+  mt.filter  = flip(filter)
+  mt.fold    = function(self, op, base)
+    return fold(op, base, self)
+  end
+  mt.take    = flip(take)
+  mt.drop    = flip(drop)
+  mt.collect = collect
 end
+
+return M

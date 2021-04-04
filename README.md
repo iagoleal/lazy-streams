974eeb9c71450d496d0279cda78ff044d5fa537c
iago <iago.lealf@gmail.com>
2021-04-04 01:51:27 -0300

    prototype version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lazy Streams in Lua
+
+A Lua library to manipulate lazy linked lists.
+Highly based on Haskell's list type.
---
+++ init.lua
@@ -0,0 +1,241 @@
+
+
+-- Constant function
+function const(x)
+  return function() return x end
+end
+
+-- Class for lazy streams
+Stream = {}
+
+-- Memoize any constructor
+local function delay(proc)
+  local already = false
+  local result  = nil
+  return function ()
+    if proc == nil then
+      return nil
+    end
+    if not already then
+      result  = proc()
+      already = true
+    end
+    return result
+  end
+end
+
+-- Force execution of constructor
+local function force(f)
+  return f()
+end
+
+------------
+-- Creation
+------------
+
+-- The empty stream
+empty = nil
+
+function isempty(s)
+  return s == nil
+end
+
+function new(val, constructor)
+  assert(type(constructor) == "function", "Function needed to build stream")
+  constructor = constructor or const(nil)
+  local t = { value = val
+            , next  = delay(constructor)
+            }
+  setmetatable(t,
+    { __type = "stream"
+    , __tostring = function(s)
+      if isempty(s) then
+        return "empty"
+      end
+      return table.concat {'stream [', tostring(s.value), ', ...]'}
+    end
+    , __index = function(s, idx)
+        return access(s, idx)
+      end
+    , __setindex = function(t, k, v)
+      error("Streams are read-only --- Try creating a new one", 2)
+    end
+    , __len   = function(s)
+        local op = function(x,y) return 1 + y end
+        return fold(op, 0, s)
+      end
+    })
+  return t
+end
+
+
+
+function isstream(s)
+  local mt = getmetatable(s)
+  return mt and mt.__type == "stream"
+end
+
+function cons(val, stream)
+  return new(val, const(stream))
+end
+-- Accessor functions
+function head(s)
+  return rawget(s, 'value')
+end
+
+function tail(s)
+  return force(s.next)
+end
+
+-- Access stream at given index.
+-- Note: stream (like lua tables) are 1-indexed.
+function access(s, idx)
+  assert(type(idx) == "number" and idx >= 1, "Stream indices start from 1")
+  if isempty(s) then
+    error("Tried to access index " .. tostring(idx) .. " of empty stream")
+  end
+  if idx == 1 then
+    return head(s)
+  else
+    return access(tail(s), idx-1)
+  end
+end
+
+-- Dismember a stream into head and tail.
+-- Note: This function forces the tail execution
+function uncons(s)
+  return head(s), tail(s)
+end
+
+--------------------------
+-- Higher Order Functions
+--------------------------
+
+-- Apply function f to all elements of a stream.
+-- If f is n-ary, you can enter n streams and it will be applied to all of them.
+function map(f, ...)
+  local streams = table.pack(...)
+  local hs = {}
+  if streams.n ==  0 then return empty end
+  for i = 1, streams.n do
+    if isempty(streams[i]) then
+      return empty
+    end
+    hs[i] = head(streams[i])
+  end
+  return new(f(table.unpack(hs)), function()
+      local ts = {}
+      for i = 1, streams.n do
+        if isempty(streams[i]) then
+          return empty
+        end
+        ts[i] = tail(streams[i])
+      end
+      return map(f, table.unpack(ts))
+  end)
+end
+
+function filter(p, s)
+  if isempty(s) then
+    return empty
+  else
+    local h = head(s)
+    if p(h) then
+      return new(h, function()
+        return filter(p, tail(h))
+      end)
+    else
+      return filter(p, tail(h))
+    end
+  end
+end
+
+function fold(op, base, s)
+  if isempty(s) then
+    return base
+  else
+    return op(head(s), fold(op, base, tail(s)))
+  end
+end
+
+-------------------
+-- Slicing streams
+-------------------
+
+-- Truncate the first n elements of a stream
+function take(n, s)
+  if isempty(s) then return empty end
+  if n == nil then
+    return s
+  end
+  if n <= 0 then
+    return empty
+  else
+    return new(head(s), function()
+      return take(n-1, tail(s))
+    end)
+  end
+end
+
+-- Drop the first n elements of a stream
+function drop(n, s)
+  if isempty(s) then return empty end
+  if n == nil then
+    return s
+  end
+  if n <= 0 then
+    return s
+  else
+    return drop(n-1, tail(s))
+  end
+end
+
+-- Collect a stream into a table.
+-- Optional argument may be used to collect only the first n elements
+function collect(s, n)
+  if n ~= nil then
+    return collect(take(n, s))
+  end
+  local values = {}
+  local i = 1
+  while not (isempty(s)) do
+    values[i] = head(s)
+    s = tail(s)
+    i = i + 1
+  end
+  return values
+end
+
+----------------
+-- Construction
+----------------
+
+-- A stream of number from i to j.
+-- Default step is 1.
+function range(i, j, step)
+  if type(j) == "number" and i > j then
+    return empty
+  end
+  step = step or 1
+  return new(i, function()
+    return range(i + step, j, step)
+  end)
+end
+
+-- Return a stream repeating the same value
+function replicate(x)
+  return new(x, function()
+    return replicate(x)
+  end)
+end
+
+pp = function(t)
+  if isstream(t) then
+    return pp(collect(t))
+  end
+  io.write('[')
+  for _, v in ipairs(t) do
+    io.write(tostring(v), ", ")
+  end
+  io.write(']\n')
+end
